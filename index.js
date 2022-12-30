82b144044856c5aac0ec27099d2c05962631fdff
Diganta Nayak <diganta.19032001@gmail.com>
2022-12-30 19:22:10 +0530

    play sound on keypress functionality added

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
+function sounds(k) {
+  switch (k) {
+    case "w":
+      var tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var kick = new Audio("./sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "l":
+      var crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
+
+    default:
+      console.log(this.innerHTML);
+      break;
+  }
+}
+
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
 
-    switch (buttonInnerHTML) {
-      case "w":
-        var tom1 = new Audio("./sounds/tom-1.mp3");
-        tom1.play();
-        break;
-      case "a":
-        var tom2 = new Audio("./sounds/tom-2.mp3");
-        tom2.play();
-        break;
-      case "s":
-        var tom3 = new Audio("./sounds/tom-3.mp3");
-        tom3.play();
-        break;
-      case "d":
-        var tom4 = new Audio("./sounds/tom-4.mp3");
-        tom4.play();
-        break;
-      case "j":
-        var snare = new Audio("./sounds/snare.mp3");
-        snare.play();
-        break;
-      case "k":
-        var kick = new Audio("./sounds/kick-bass.mp3");
-        kick.play();
-        break;
-      case "l":
-        var crash = new Audio("./sounds/crash.mp3");
-        crash.play();
-        break;
-
-      default:
-        console.log(this.innerHTML);
-        break;
-    }
+    sounds(buttonInnerHTML);
   });
 }
+
+document.addEventListener("keydown", function (event) {
+  var keyPressed = event.key;
+  sounds(keyPressed);
+});
